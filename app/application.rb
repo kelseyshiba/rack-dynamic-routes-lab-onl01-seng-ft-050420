0156b4a0fee0973292689e18ccbbf19fb5d6e869
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-08 18:35:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,7 @@ class Application
 
     if req.path.match(/items/)
       user_item = req.path.split("/items/").last
-      if found_item = @@items.collect {|item| item.name == user_item}
-        binding.pry
+      if found_item = @@items.find {|item| item.name == user_item}
         found_item.each {|item| resp.write("#{item.price}")}
         resp.status = 200
       else 
